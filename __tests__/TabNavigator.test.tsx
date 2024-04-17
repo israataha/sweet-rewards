5beb8816a05998fb8534229909731c3969ecb984
israataha <israataha@gmail.com>
2024-04-17 17:58:28 -0500

    Change Earn tab to Scan and add initial UI

--- __tests__/TabNavigator.test.tsx
+++ __tests__/TabNavigator.test.tsx
@@ -1,14 +1,39 @@
 import * as React from 'react';
-import { screen } from '@testing-library/react-native';
+import { fireEvent, screen } from '@testing-library/react-native';
 import { TabNavigator } from '../src/navigators/TabNavigator';
 import { renderNavigator } from './utils/test-utils';
 
-test('Correctly rendering tabs', () => {
+test('Correctly renders tabs', async () => {
   renderNavigator(<TabNavigator />);
 
   // React Navigation uses `button` role for tab buttons
-  expect(screen.getByRole('button', { name: 'Rewards' })).toBeOnTheScreen();
-  expect(screen.getByRole('button', { name: 'Earn' })).toBeOnTheScreen();
-  expect(screen.getByRole('button', { name: 'Locations' })).toBeOnTheScreen();
-  expect(screen.getByRole('button', { name: 'More' })).toBeOnTheScreen();
+  expect(await screen.findByRole('button', { name: 'Rewards' })).toBeOnTheScreen();
+  expect(await screen.findByRole('button', { name: 'Scan' })).toBeOnTheScreen();
+  expect(await screen.findByRole('button', { name: 'Locations' })).toBeOnTheScreen();
+  expect(await screen.findByRole('button', { name: 'More' })).toBeOnTheScreen();
+});
+
+test('Correctly changes tabs', async () => {
+  renderNavigator(<TabNavigator />);
+
+  const scanTab = await screen.findByRole('button', { name: 'Scan' });
+  const locationsTab = await screen.findByRole('button', { name: 'Locations' });
+  const moreTab = await screen.findByRole('button', { name: 'More' });
+
+  expect(await screen.findByRole('header', { name: 'Rewards' })).toBeOnTheScreen();
+
+  fireEvent.press(scanTab);
+
+  expect(await screen.findByRole('header', { name: 'Scan' })).toBeOnTheScreen();
+  expect(screen.queryByRole('header', { name: 'Rewards' })).not.toBeOnTheScreen();
+
+  fireEvent.press(locationsTab);
+
+  expect(await screen.findByRole('header', { name: 'Locations' })).toBeOnTheScreen();
+  expect(screen.queryByRole('header', { name: 'Scan' })).not.toBeOnTheScreen();
+
+  fireEvent.press(moreTab);
+
+  expect(await screen.findByRole('header', { name: 'More' })).toBeOnTheScreen();
+  expect(screen.queryByRole('header', { name: 'Locations' })).not.toBeOnTheScreen();
 });
---
+++ src/navigators/AccountNavigator.tsx
@@ -0,0 +1,10 @@
+// const AccountStack = createNativeStackNavigator();
+
+// function ProfileStackScreen() {
+//   return (
+//     <ProfileStack.Navigator>
+//       <ProfileStack.Screen name="Profile" component={ProfileScreen} />
+//       {/* other screens */}
+//     </ProfileStack.Navigator>
+//   );
+// }
