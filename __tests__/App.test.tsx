94c83f331cb6d04fc69c39134cf519181a5a00a8
israataha <israataha@gmail.com>
2024-04-17 19:10:26 -0500

    Resolve test console.error Warning: An update to ForwardRef inside a test was not wrapped in act(...)

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -3,14 +3,16 @@
  */
 
 import * as React from 'react';
-import { render, screen } from '@testing-library/react-native';
+import { act, render, screen } from '@testing-library/react-native';
 import App from '../App';
 
 test('App renders correctly', async () => {
   render(<App />);
 
-  expect(await screen.findByRole('button', { name: 'Rewards' })).toBeOnTheScreen();
-  expect(await screen.findByRole('button', { name: 'Scan' })).toBeOnTheScreen();
-  expect(await screen.findByRole('button', { name: 'Locations' })).toBeOnTheScreen();
-  expect(await screen.findByRole('button', { name: 'More' })).toBeOnTheScreen();
+  await act(async () => {
+    expect(await screen.findByRole('button', { name: 'Rewards' })).toBeOnTheScreen();
+    expect(await screen.findByRole('button', { name: 'Scan' })).toBeOnTheScreen();
+    expect(await screen.findByRole('button', { name: 'Locations' })).toBeOnTheScreen();
+    expect(await screen.findByRole('button', { name: 'More' })).toBeOnTheScreen();
+  });
 });
--- __tests__/TabNavigator.test.tsx
+++ __tests__/TabNavigator.test.tsx
@@ -1,39 +1,45 @@
 import * as React from 'react';
-import { fireEvent, screen } from '@testing-library/react-native';
+import { act, fireEvent, screen } from '@testing-library/react-native';
 import { TabNavigator } from '../src/navigators/TabNavigator';
 import { renderNavigator } from './utils/test-utils';
 
 test('Correctly renders tabs', async () => {
   renderNavigator(<TabNavigator />);
 
-  // React Navigation uses `button` role for tab buttons
-  expect(await screen.findByRole('button', { name: 'Rewards' })).toBeOnTheScreen();
-  expect(await screen.findByRole('button', { name: 'Scan' })).toBeOnTheScreen();
-  expect(await screen.findByRole('button', { name: 'Locations' })).toBeOnTheScreen();
-  expect(await screen.findByRole('button', { name: 'More' })).toBeOnTheScreen();
+  await act(async () => {
+    // React Navigation uses `button` role for tab buttons
+    expect(await screen.findByRole('button', { name: 'Rewards' })).toBeOnTheScreen();
+    expect(await screen.findByRole('button', { name: 'Scan' })).toBeOnTheScreen();
+    expect(await screen.findByRole('button', { name: 'Locations' })).toBeOnTheScreen();
+    expect(await screen.findByRole('button', { name: 'More' })).toBeOnTheScreen();
+  });
 });
 
 test('Correctly changes tabs', async () => {
   renderNavigator(<TabNavigator />);
 
-  const scanTab = await screen.findByRole('button', { name: 'Scan' });
-  const locationsTab = await screen.findByRole('button', { name: 'Locations' });
-  const moreTab = await screen.findByRole('button', { name: 'More' });
+  await act(async () => {
+    expect(await screen.findByRole('header', { name: 'Rewards' })).toBeOnTheScreen();
+  });
 
-  expect(await screen.findByRole('header', { name: 'Rewards' })).toBeOnTheScreen();
+  fireEvent.press(await screen.findByRole('button', { name: 'Scan' }));
 
-  fireEvent.press(scanTab);
+  await act(async () => {
+    expect(await screen.findByRole('header', { name: 'Scan' })).toBeOnTheScreen();
+    expect(screen.queryByRole('header', { name: 'Rewards' })).not.toBeOnTheScreen();
+  });
 
-  expect(await screen.findByRole('header', { name: 'Scan' })).toBeOnTheScreen();
-  expect(screen.queryByRole('header', { name: 'Rewards' })).not.toBeOnTheScreen();
+  fireEvent.press(await screen.findByRole('button', { name: 'Locations' }));
 
-  fireEvent.press(locationsTab);
+  await act(async () => {
+    expect(await screen.findByRole('header', { name: 'Locations' })).toBeOnTheScreen();
+    expect(screen.queryByRole('header', { name: 'Scan' })).not.toBeOnTheScreen();
+  });
 
-  expect(await screen.findByRole('header', { name: 'Locations' })).toBeOnTheScreen();
-  expect(screen.queryByRole('header', { name: 'Scan' })).not.toBeOnTheScreen();
+  fireEvent.press(await screen.findByRole('button', { name: 'More' }));
 
-  fireEvent.press(moreTab);
-
-  expect(await screen.findByRole('header', { name: 'More' })).toBeOnTheScreen();
-  expect(screen.queryByRole('header', { name: 'Locations' })).not.toBeOnTheScreen();
+  await act(async () => {
+    expect(await screen.findByRole('header', { name: 'More' })).toBeOnTheScreen();
+    expect(screen.queryByRole('header', { name: 'Locations' })).not.toBeOnTheScreen();
+  });
 });
