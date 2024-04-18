7851044da539adba415d547550debf409ecf2de5
israataha <israataha@gmail.com>
2024-04-17 21:09:25 -0500

    Added Sodo Sans custom fonts

---
+++ src/theme/typography.ts
@@ -0,0 +1,10 @@
+export const fonts = {
+  SodoSans: {
+    light: 'SodoSans-Light',
+    thin: 'SodoSans-Thin',
+    regular: 'SodoSans-Regular',
+    bold: 'SodoSans-Bold',
+    semibold: 'SodoSans-SemiBold',
+    black: 'SodoSans-Black',
+  },
+};
